b33fbf3ca2aabc156dcb653d12f7b8efece70aed
ElainePark2007 <eapark@nic.edu>
2025-03-25 13:43:23 -0700

    Elaine branch test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+int addThree(int initial);
+
 int main()
 {
     std::cout << "Hello, World!\n";
+    std::cout << "Type a number: ";
+    int num;
+    std::cin >> num;
+    std::cout << num << " + 3 = " << addThree(num) << '\n';
+    return 0;
+}
+
+int addThree(int initial)
+{
+    int result = initial + 3;
+    return result;
 }
